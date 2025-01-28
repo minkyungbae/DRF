43dc221c630a1d25de2d83fa83336a3bb2695892
minkyungbae <bmk0703@naver.com>
2025-01-28 20:06:52 +0900

    Add json-2 url

--- articles/urls.py
+++ articles/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "articles"
 urlpatterns = [
     path("html/", views.article_list_html, name="article_list_html"),
+    path("json-01/", views.json_01, name="json_01"),
+    path("json-02/", views.json_02, name="json_02"),
 ]
