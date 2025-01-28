6d58129b3ae43cf89c98f137cb51fc465c6fa330
minkyungbae <bmk0703@naver.com>
2025-01-28 19:10:55 +0900

    Add article_list_html

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+def article_list_html(request):
+    articles = Article.objects.all()
+    context = {"articles":articles}
+    return render(request, "articles/article_list.html", context)
