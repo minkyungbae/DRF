296a40af879bb5fc25f43edc3c8ad5f643e2e91e
minkyungbae <bmk0703@naver.com>
2025-01-30 18:20:50 +0900

    Add Comment model

--- articles/models.py
+++ articles/models.py
@@ -5,3 +5,13 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    # 1. 외래 키로, 댓글이 어느 글(Article)에 속하는지 알려줌
+    # 2. Article 모델과 Comment 모델이 1:N의 관계를 가짐
+    # 3. on_delete=models.CASCADE -> Article이 삭제되면, 댓글도 함께 삭제
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) # 댓글이 처음 생성됐을 때의 시간 기록
+    updated_at = models.DateTimeField(auto_now=True) # 댓글이 수정될 때마다 시간 기록
