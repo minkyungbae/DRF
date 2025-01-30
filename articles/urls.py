925e0b0b7119dbfc059cdf3711c8240d6768b878
minkyungbae <bmk0703@naver.com>
2025-01-30 18:45:40 +0900

    Add comment_list

--- articles/urls.py
+++ articles/urls.py
@@ -5,4 +5,9 @@ app_name = "articles"
 urlpatterns = [
     path("", views.ArticleListAPIView.as_view()),
     path("<int:pk>/", views.ArticleDetailAPIView.as_view()),
+    
+    # 특정 Article의 댓글 조회
+    path("<int:article_pk>/comments/",
+         views.CommentListAPIView.as_view(),
+         name="comment_list"),
 ]
