f5906d6c837760cb7a744e60f263580441614294
minkyungbae <bmk0703@naver.com>
2025-01-31 12:59:43 +0900

    Add class ArticleDetailSerializer

--- articles/serializers.py
+++ articles/serializers.py
@@ -14,15 +14,14 @@ class CommentSerializer(serializers.ModelSerializer):
         return ret
 
 class ArticleSerializer(serializers.ModelSerializer):
-    # 모든 comments들을 CommentSerializer에 넘겨서 직렬화한 후, fields 추가해 줘!
-    comments = CommentSerializer(many=True, read_only=True) # 오버라이딩
-    # 댓글 갯수
-    comments_count = serializers.IntegerField(source="comments.count", read_only=True) #.count가 ORM 함수
-    
     class Meta:
         model = Article
         fields = "__all__"
         
 
-
+class ArticleDetailSerializer(ArticleSerializer):
+    # 모든 comments들을 CommentSerializer에 넘겨서 직렬화한 후, fields 추가해 줘!
+    comments = CommentSerializer(many=True, read_only=True) # 오버라이딩
+    # 댓글 갯수
+    comments_count = serializers.IntegerField(source="comments.count", read_only=True) #.count가 ORM 함수    
         
