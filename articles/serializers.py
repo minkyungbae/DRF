f2dd9daf5ff4e166b058d46188b426ddcb9cf5b4
minkyungbae <bmk0703@naver.com>
2025-01-31 12:27:48 +0900

    Add comments in ArticleSerializer

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
 from .models import Article, Comment
 
-
-class ArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = "__all__"
-        
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = "__all__"
         read_only_fields = ("article",)
+
+class ArticleSerializer(serializers.ModelSerializer):
+    # 모든 comments들을 CommentSerializer에 넘겨서 직렬화한 후, fields 추가해 줘!
+    comments = CommentSerializer(many=True, read_only=True) # 오버라이딩
+    
+    class Meta:
+        model = Article
+        fields = "__all__"
+        
+
+
+        
