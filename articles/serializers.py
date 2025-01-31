7f34956cc39ee58d6e65d4f1d49b55afea9b7108
minkyungbae <bmk0703@naver.com>
2025-01-31 12:49:08 +0900

    Add comments_count

--- articles/serializers.py
+++ articles/serializers.py
@@ -10,6 +10,8 @@ class CommentSerializer(serializers.ModelSerializer):
 class ArticleSerializer(serializers.ModelSerializer):
     # 모든 comments들을 CommentSerializer에 넘겨서 직렬화한 후, fields 추가해 줘!
     comments = CommentSerializer(many=True, read_only=True) # 오버라이딩
+    # 댓글 갯수
+    comments_count = serializers.IntegerField(source="comments.count", read_only=True) #.count가 ORM 함수
     
     class Meta:
         model = Article
