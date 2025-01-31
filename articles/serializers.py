354cb4de3b37caa4cd3c5d6cc5bd5c710784d347
minkyungbae <bmk0703@naver.com>
2025-01-31 12:53:46 +0900

    Add def to_represnetation in CommentSerializer

--- articles/serializers.py
+++ articles/serializers.py
@@ -6,6 +6,12 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = "__all__"
         read_only_fields = ("article",)
+        
+    # 댓글 목록에서 article 없애기
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret.pop("article")
+        return ret
 
 class ArticleSerializer(serializers.ModelSerializer):
     # 모든 comments들을 CommentSerializer에 넘겨서 직렬화한 후, fields 추가해 줘!
