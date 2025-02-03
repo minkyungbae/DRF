12a59d72cb56c4ae2bd2d3dd856f056c333401a0
minkyungbae <bmk0703@naver.com>
2025-02-03 14:30:12 +0900

    역참조 추가함

--- articles/views.py
+++ articles/views.py
@@ -106,17 +106,33 @@ class CommentDetailAPIView(APIView):
             return Response(serializer.data)
 
 
-# 정참조 조회하기
+# # 정참조 조회하기
+# @api_view(["GET"])
+# def check_sql(request):
+#     from django.db import connection
+
+#     comments = Comment.objects.all().select_related("article")
+#     for comment in comments:
+#         print(comment.article.title)
+
+#     print("-" * 30)
+#     print(connection.queries)
+
+#     return Response()
+
+
+# 역참조 조회하기
 @api_view(["GET"])
 def check_sql(request):
     from django.db import connection
 
-    comments = Comment.objects.all().select_related("article")
+    articles = Article.objects.all().prefetch_related("comments")
+    for article in articles:
+        comments = article.comments.all()
     for comment in comments:
-        print(comment.article.title)
+        print(comment.id)
 
     print("-" * 30)
     print(connection.queries)
 
     return Response()
-
