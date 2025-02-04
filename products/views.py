b19347d27171f4bcb132ff6f80716a3e353cbde6
minkyungbae <bmk0703@naver.com>
2025-02-04 12:09:51 +0900

    redis 입력하기 전 code

--- products/views.py
+++ products/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(["GET"])
 def product_list(request):
-    return Response()
+    products = Product.pbjects.all()
+    serializer = ProductSerializer(products, many=True)
+    json_reponse = serializer.data
+    return Response(json_reponse)
