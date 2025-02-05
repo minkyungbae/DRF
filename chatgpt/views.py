ec30741670be676da19e553c49560b77c66c40de
minkyungbae <bmk0703@naver.com>
2025-02-05 17:37:16 +0900

    Add TranslateAPIview

--- chatgpt/views.py
+++ chatgpt/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .bots import translate_bot
 
-# Create your views here.
+class TranslateAPIView():
+    
+    def post(self, request):
+        data = request.data # request 받은 data를 data에 넣기
+        message = data.get("message", "") # message는 data에 있는 "message" 값 들고 오기
+        translated_message = translate_bot(message) # 번역할 메시지는 translate_bot에 있는 message 로직으로 하기
+        return Response({"translated_message": translated_message})
+        
