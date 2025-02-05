7a449333b5166e8a96374b8bd7d8e1b7fb63e9bc
minkyungbae <bmk0703@naver.com>
2025-02-05 16:34:41 +0900

    Add 여러번 대화할 수 있는 코드

--- openai_test.py
+++ openai_test.py
@@ -1,29 +1,49 @@
 from openai import OpenAI
 from api_pjt import config
 
-client = OpenAI(
+CLIENT = OpenAI(
     api_key=config.OPENAI_API_KEY,
 )
 
+
+def ask_to_gpt(instructions, message):
+    completion = CLIENT.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": instructions,
+            },
+            {
+                "role": "user",
+                "content": message,
+            },
+        ],
+    )
+    return completion.choices[0].message.content
+
+
 system_instructions = """
-이제부터 너는 Django 프레임워크에 대해 설명하고 
-사용자가 Django 프레임워크에 대해 어려움을 겪고 있다고 가정하고 도와주는 챗봇이 되어야해.
-다른 코딩 언어나 프레임워크에 대해 설명하거나 다른 주제로 이야기하는 것은 금지야.
-Django 공식문서의 링크를 제공하거나 Django 프레임워크에 대한 설명도 추가해줘.
+이제부터 너는 '에이든 카페'의 직원이야.
+아래 종류의 음료 카테고리에서 주문을 받고, 주문을 처리하는 대화를 진행해.
+
+1. 아메리카노
+2. 카페라떼
+3. 프라푸치노
+4. 콜드브루
+5. 스무디
+
+주문을 받으면, 주문 내용을 확인하고, 주문을 처리하는 대화를 진행해.
+주문이 완료되면, 주문 내용을 확인하고, 주문이 완료되었음을 알려줘.
 """
 
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": system_instructions,
-        },
-        {
-            "role": "user",
-            "content": "Django가 너무 어려워요. 도와... 아니 살려주세요.",
-        },
-    ],
-)
+# 처음 인사를 위해
+response = ask_to_gpt(system_instructions, "")
+print(f"에이든 카페봇 : {response}\n\n")
 
-print(completion.choices[0].message)
+while True:
+    user_message = input("유저 : ")
+    if user_message == "종료":
+        break
+    response = ask_to_gpt(system_instructions, user_message)
+    print(f"에이든 카페봇 : {response}\n\n")
