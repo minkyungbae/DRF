3d81d691048920fd070c19cfedf8f5200789c1b2
minkyungbae <bmk0703@naver.com>
2025-02-05 16:38:24 +0900

    조건 수정

--- openai_test.py
+++ openai_test.py
@@ -24,14 +24,14 @@ def ask_to_gpt(instructions, message):
 
 
 system_instructions = """
-이제부터 너는 '에이든 카페'의 직원이야.
+이제부터 너는 '민경 카페'의 직원이야.
 아래 종류의 음료 카테고리에서 주문을 받고, 주문을 처리하는 대화를 진행해.
 
-1. 아메리카노
-2. 카페라떼
-3. 프라푸치노
-4. 콜드브루
-5. 스무디
+1. 초코라떼
+2. 고구마라떼
+3. 밤라떼
+4. 뱅쇼
+5. 딸기주스
 
 주문을 받으면, 주문 내용을 확인하고, 주문을 처리하는 대화를 진행해.
 주문이 완료되면, 주문 내용을 확인하고, 주문이 완료되었음을 알려줘.
@@ -39,11 +39,11 @@ system_instructions = """
 
 # 처음 인사를 위해
 response = ask_to_gpt(system_instructions, "")
-print(f"에이든 카페봇 : {response}\n\n")
+print(f"민경 카페봇 : {response}\n\n")
 
 while True:
     user_message = input("유저 : ")
     if user_message == "종료":
         break
     response = ask_to_gpt(system_instructions, user_message)
-    print(f"에이든 카페봇 : {response}\n\n")
+    print(f"민경 카페봇 : {response}\n\n")
